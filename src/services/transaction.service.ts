2afbac847f842379ae05b09c6dd6de4e53b66094
Eduardo Vergara� <eduver_san@hotmail.com>
2025-06-30 12:46:14 +0100

    Actualiza los servicios de balance y transacciones para incluir el tipo de moneda como parámetro, mejorando la flexibilidad en la obtención de datos financieros. Se importa el enum MoneyTypes para su uso en ambas funciones.

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,15 +1,17 @@
 import { supabase } from "@/lib/supabase";
 import { getUser } from "./auth.service";
+import { MoneyTypes } from "@/model/money-types.enum";
 
-export async function getTransactions(startDate: string, endDate: string) {
+export async function getTransactions(startDate: string, endDate: string, moneyType: MoneyTypes = MoneyTypes.EUR) {
     const { data: user } = await getUser();
     if (!user) {
         return { data: null, error: "User not found" };
-    }   
+    }
     const { data, error } = await supabase.from("expenses").select()
-    .filter("userId", "eq", user.id).filter("date", "gte", startDate)
-    // .filter("date", "lte", endDate)
-    .order("date", { ascending: false });
+        .filter("userId", "eq", user.id).filter("date", "gte", startDate)
+        // .filter("date", "lte", endDate)
+        .filter("money", "eq", moneyType)
+        .order("date", { ascending: false });
 
     return { data, error };
 }
