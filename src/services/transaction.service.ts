dab96657a12c4aabe19de5866312e0c423bb396d
Eduardo Vergara� <eduver_san@hotmail.com>
2025-07-01 13:28:09 +0100

    feat: :sparkles: Agrega función para guardar transacciones en el servicio de transacciones, incluyendo validación de usuario y manejo de errores.

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -16,3 +16,20 @@ export async function getTransactions(startDate: string, endDate: string, moneyT
     return { data, error };
 }
 
+export async function saveTransaction(input: { amount: number, money: MoneyTypes, description: string, observation?: string }) {
+    const { data: user } = await getUser();
+    if (!user) {
+        return { data: null, error: "UNAUTHORIZED" };
+    }
+    const { data, error } = await supabase.from("expenses").insert({
+        cost: input.amount,
+        money: input.money,
+        userId: user.id,
+        date: new Date().toISOString(),
+        description: input.description,
+        observation: input.observation
+    }).select()
+    console.log(data, error);
+    return { data, error };
+}
+
