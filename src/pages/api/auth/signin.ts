0f6595a2745d0f59e5908804f69eaf1262cd78bb
Eduardo Vergara� <eduver_san@hotmail.com>
2025-07-04 12:59:34 +0100

    feat: :sparkles: Refactoriza la función de redirección en el servicio de autenticación para utilizar una URL dinámica basada en el entorno, mejorando la flexibilidad en la configuración del entorno de producción. Además, elimina la propiedad server:defer de los componentes UpcomingBills y Transactions para optimizar la carga de datos en el dashboard.

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -3,6 +3,18 @@ import { supabase } from "@/lib/supabase";
 import type { Provider } from "@supabase/supabase-js";
 import { routes } from "@/model/routes";
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:4321/'
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith('/') ? url : `${url}/`
+  return url
+}
+
 export const POST: APIRoute = async ({ request, cookies, redirect, url }) => {
   const formData = await request.formData();
   const email = formData.get("email")?.toString();
@@ -15,7 +27,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect, url }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider as Provider,
       options: {
-        redirectTo: `${url.origin}/api/auth/callback`,
+        redirectTo: `${getURL()}/api/auth/callback`,
       },
     });
 
